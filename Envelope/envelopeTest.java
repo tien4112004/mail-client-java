5122e66122ba40117abbeb28cb76d4d9565f19b7
Ly'TTin <22120371@student.hcmus.edu.vn>
2023-11-17 21:16:33 +0700

    Envelope

---
+++ Envelope/envelopeTest.java
@@ -0,0 +1,8 @@
+package Envelope;
+
+public class envelopeTest {
+    public static void main(String[] args) {
+        envelope envelope = new envelope();
+        System.out.println(envelope.toString());
+    }
+}
--- Message/MessageTest.java
+++ Message/MessageTest.java
@@ -1,3 +1,5 @@
+package message;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
