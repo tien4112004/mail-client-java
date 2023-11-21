b7f93976cf3fbde2d82b8b637047a6db28afb500
Tien Phan Thanh <tien4112004@gmail.com>
2023-11-21 22:45:28 +0700

    add more unit test for Message

--- src/test/java/Message/MessageTest.java
+++ src/test/java/Message/MessageTest.java
@@ -1,9 +1,6 @@
 package Message;
 
 import org.junit.jupiter.api.Test;
-
-import Message.Message;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageTest {
@@ -22,6 +19,61 @@ class MessageTest {
         assertEquals(sender, message.getSender());
         assertArrayEquals(new String[] { recipientsTo[0], recipientsTo[1], recipientsCc[0], recipientsCc[1],
                 recipientsBcc[0], recipientsBcc[1] }, message.getRecipients());
+        assertEquals(subject, message.getSubject());
+        // assertEquals(content, message.getContent());
+        // assertArrayEquals(attachments, message.getAttachments());
+    }
+
+    @Test
+    void testMessageConstructorWithEmptyRecipients() {
+        String sender = "sender@localhost";
+        String[] recipientsTo = {};
+        String[] recipientsCc = {};
+        String[] recipientsBcc = {};
+        String subject = "Test Email";
+        String content = "This is a test email";
+        String[] attachments = {};
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Message(sender, recipientsTo, recipientsCc, recipientsBcc, subject, content, attachments);
+        });
+    }
+
+    @Test
+    void testMessageConstructorWithAttachments() {
+        String sender = "sender@localhost";
+        String[] recipientsTo = { "recipientTo1@localhost", "recipientTo2@localhost" };
+        String[] recipientsCc = { "recipientCc1@localhost", "recipientCc2@localhost" };
+        String[] recipientsBcc = { "recipientBcc1@localhost", "recipientBcc2@localhost" };
+        String subject = "Test Email";
+        String content = "This is a test email";
+        String[] attachments = { "attachments/test.cpp", "attachments/test.txt" };
+
+        Message message = new Message(sender, recipientsTo, recipientsCc, recipientsBcc, subject, content, attachments);
+
+        assertEquals(sender, message.getSender());
+        assertArrayEquals(new String[] { recipientsTo[0], recipientsTo[1], recipientsCc[0], recipientsCc[1],
+                recipientsBcc[0], recipientsBcc[1] }, message.getRecipients());
+        assertEquals(subject, message.getSubject());
+        // assertEquals(content, message.getContent());
+        // assertArrayEquals(attachments, message.getAttachments());
+        System.out.println(message.toString());
+    }
+
+    @Test
+    void testMessageConstructorWithInvalidAttachment() {
+        String sender = "sender@localhost";
+        String[] recipientsTo = { "recipientTo1@localhost", "recipientTo2@localhost" };
+        String[] recipientsCc = { "recipientCc1@localhost", "recipientCc2@localhost" };
+        String[] recipientsBcc = { "recipientBcc1@localhost", "recipientBcc2@localhost" };
+        String subject = "Test Email";
+        String content = "This is a test email";
+        String[] attachments = { "path/to/nonexistent/attachment" };
+
+        // assertThrows(Exception.class, () -> {
+        // new Message(sender, recipientsTo, recipientsCc, recipientsBcc, subject,
+        // content, attachments);
+        // });
     }
 
     @Test
@@ -38,17 +90,5 @@ class MessageTest {
 
         assertArrayEquals(new String[] { recipientsTo[0], recipientsTo[1], recipientsCc[0], recipientsCc[1],
                 recipientsBcc[0], recipientsBcc[1] }, message.getRecipients());
-    }
-
-    @Test
-    void testBuildRecipientString() {
-        String[] recipients = { "recipient1@localhost", "recipient2@localhost" };
-        Message message = new Message("sender@localhost", recipients, new String[] {}, new String[] {}, "Test Email",
-                "This is a test email", new String[] {});
-
-        String expected = "recipient1@localhost,recipient2@localhost";
-        String actual = message.buildRecipientString(recipients);
-
-        assertEquals(expected, actual);
     }
 }
