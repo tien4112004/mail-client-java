a2e1b44fba9fb712e68692055c5a3fd5f2e3a61c
Tien Phan Thanh <tien4112004@gmail.com>
2023-12-17 20:23:33 +0700

    fix POP3: close connection after retrieve

--- src/main/java/Socket/POP3Socket.java
+++ src/main/java/Socket/POP3Socket.java
@@ -1,12 +1,15 @@
 package Socket;
 
 import java.io.IOException;
+import java.net.PasswordAuthentication;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 import java.io.FileReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -197,13 +200,24 @@ public class POP3Socket extends MailSocket {
             String rawMessage = RETR(i + 1 + "");
             // Files.write(Paths.get("./test.msg"), rawMessage.getBytes());
             MessageParser parser = new MessageParser();
-            parser.parse(rawMessage);
-            Message email = parser.createMessage();
-            email.saveMail(messagesID[i]);
+            // parser.parse(rawMessage);
+            // Message email = parser.createMessage();
+            // email.saveMail(messagesID[i]);
+            saveEmail("Inbox/" + messagesID[i], rawMessage);
             // TODO: filter
             messageList.add(messageObject);
         }
         writeMessageStatus.writeJSON(messageList);
+        quit();
+    }
+
+    private void saveEmail(String filename, String rawMessage) {
+        try {
+            Path emailPath = Paths.get(filename);
+            Files.write(emailPath, rawMessage.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void deleteMessage(String messageOrder) throws IOException {
