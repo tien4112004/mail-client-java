43e460aacaa6e9ccb86709b01783a864431d3b38
imtrongtin <22120371@student.hcmus.edu.vn>
2023-12-24 08:30:14 +0700

    remove comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,15 @@ import Socket.POP3Socket;
 import UI.MainMenu;
 
 class Multithreading extends Thread {
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_TEXT_YELLOW = "\u001B[33m";
+    // private static final String ANSI_RESET = "\u001B[0m";
+    // private static final String ANSI_TEXT_YELLOW = "\u001B[33m";
     private String server;
     private int port;
     private String username;
     private String password;
 
     private POP3Socket pop3Socket;
-    
+
     public Multithreading(String server, int port, String username, String password) {
         this.server = server;
         this.port = port;
@@ -26,8 +26,7 @@ class Multithreading extends Thread {
 
     @Override
     public void run() {
-        // TODO: loop with condition.
-        while (true){
+        while (true) {
             try {
                 // System.out.println(ANSI_TEXT_YELLOW + "Retrieving message..." + ANSI_RESET);
                 pop3Socket = new POP3Socket(server, port, username, password);
@@ -36,7 +35,7 @@ class Multithreading extends Thread {
                 e.printStackTrace();
             }
             try {
-                sleep(10000);  
+                sleep(10000);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -45,19 +44,7 @@ class Multithreading extends Thread {
 }
 
 public class Main {
-    // private static final String ANSI_RESET = "\u001B[0m";
-    // private static final String ANSI_TEXT_YELLOW = "\u001B[33m";
-
     public static void main(String[] args) {
-        // List<Mailbox> mailboxes = new ArrayList<Mailbox>(
-        //         Arrays.asList(new Mailbox("Inbox"), new Mailbox("Sent"), new Mailbox("Spam"))); // load from config
-        // POP3Socket pop3Socket = new POP3Socket("localhost", 3335, ui.username, ui.password);
-        // try {
-        //     System.out.println(ANSI_TEXT_YELLOW + "Retrieving message..." + ANSI_RESET);
-        //     pop3Socket.retrieveMessage();
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
         MainMenu ui = new MainMenu();
         Multithreading reloadInterval = new Multithreading("localhost", 3335, ui.username, ui.password);
         reloadInterval.start();
